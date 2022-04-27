764e743661d3e77b88d2c2cbaffae725acfdf5b2
Cukhoaito <ngobac20031016@gmail.com>
2022-04-27 18:52:14 +0700

    Create a new Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Human
+{
+    private string? HoVaTen;
+    private float ChieuCao, CanNang;
+
+    public Human(string HT, float CC, float CN)
+    {
+        HoVaTen = HT;
+        ChieuCao = CC;
+        CanNang = CN;
+        count++;
+    }
+
+    public void GetHuman()
+    {
+        Console.WriteLine("Ten: {0} Chieu Cao: {1} Can Nang: {2}", HoVaTen, ChieuCao, CanNang);
+    }
+
+    public static int count = 0;
+
+}
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Human nguoi = new Human("Bac", 165, 65);
+            nguoi.GetHuman();
+            Console.WriteLine(Human.count);
+
+
+
+        }
+
+    }
+}
