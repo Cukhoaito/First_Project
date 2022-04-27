418a9a607761a414c078f0f323009b1e6be175d4
Cukhoaito <ngobac20031016@gmail.com>
2022-04-27 19:36:41 +0700

    Change more

--- Program.cs
+++ Program.cs
@@ -14,13 +14,58 @@ class Human
         CanNang = CN;
         count++;
     }
-
+    public float CANNANG
+    {
+        get { return CanNang; }
+        set { CanNang = value; }
+    }
     public void GetHuman()
     {
         Console.WriteLine("Ten: {0} Chieu Cao: {1} Can Nang: {2}", HoVaTen, ChieuCao, CanNang);
     }
 
     public static int count = 0;
+
+}
+
+static class Number
+{
+    private static int number;
+    public static int GetNumber()
+    {
+        return number;
+    }
+    static Number()
+    {
+        DateTime now = DateTime.Now;
+        switch (now.DayOfWeek)
+        {
+
+            case DayOfWeek.Monday:
+                number = 2;
+                break;
+            case DayOfWeek.Tuesday:
+                number = 3;
+                break;
+            case DayOfWeek.Wednesday:
+                number = 4;
+                break;
+            case DayOfWeek.Thursday:
+                number = 5;
+                break;
+            case DayOfWeek.Friday:
+                number = 6;
+                break;
+            case DayOfWeek.Saturday:
+                number = 7;
+                break;
+            case DayOfWeek.Sunday:
+                number = 8;
+                break;
+
+        }
+    }
+
 
 }
 namespace HelloWorld
@@ -32,11 +77,11 @@ namespace HelloWorld
         {
 
 
-            Human nguoi = new Human("Bac", 165, 65);
+            Human nguoi = new Human("bac", 165, 65);
             nguoi.GetHuman();
-            Console.WriteLine(Human.count);
-
-
+            nguoi.CANNANG = 70;
+            nguoi.GetHuman();
+            Number.GetNumber();
 
         }
 
